1f874539d46511ede8d2b76d4f1c840497feaeb3
Choi Yong Seok <dydtjr1994@gmail.com>
2020-01-07 16:51:56 +0900

    MFC study initial commit

---
+++ MFCExample/MFCExampleDlg.h
@@ -0,0 +1,40 @@
+﻿
+// MFCExampleDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCExampleDlg 대화 상자
+class CMFCExampleDlg : public CDialogEx
+{
+private:	
+// 생성입니다.
+public:
+	CMFCExampleDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCEXAMPLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int progress;
+	CButton CrawllerButton;
+	afx_msg void OnBnClickedStartCrawllerButton();
+	CProgressCtrl MainProgressbar;
+};
---
+++ MFCExample/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCExample.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCEXAMPLE_DIALOG           102
+#define IDS_START_CRAWLLER              102
+#define IDR_MAINFRAME                   128
+#define IDC_START_CRAWLLER_BUTTON       1000
+#define IDC_PROGRESS1                   1001
+#define IDC_MFCCOLORBUTTON1             1002
+#define IDC_MFCSHELLTREE1               1003
+#define IDC_MFCMENUBUTTON1              1004
+#define IDC_MFCFONTCOMBO1               1005
+#define IDC_MFCEDITBROWSE1              1006
+#define IDC_MFCBUTTON1                  1007
+#define IDC_COMMAND1                    1008
+#define IDC_MONTHCALENDAR1              1010
+#define IDC_PROGRESS3                   1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
